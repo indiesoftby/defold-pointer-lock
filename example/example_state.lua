fee17acf1e13721e0b21eb930a20b059ca5cbfd9
Artsiom Trubchyk <aglitchman@gmail.com>
2021-10-03 21:50:37 +0300

    Update mouse look

---
+++ example/example_state.lua
@@ -0,0 +1,13 @@
+local M = {
+    camera_pos = vmath.vector3(0, 800, 1200),
+    eye_pos = vmath.vector3(0, 70, 0),
+    yaw = 270,
+    pitch = 0,
+
+    key_forward = false,
+    key_backward = false,
+    key_step_left = false,
+    key_step_right = false,
+}
+
+return M
