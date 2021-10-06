687468817509921318a7d3e26944cb52a32c646d
Artsiom Trubchyk <aglitchman@gmail.com>
2021-10-06 16:52:30 +0300

    Update pointer locking logic

--- example/example_state.lua
+++ example/example_state.lua
@@ -1,8 +1,7 @@
 local M = {
-    camera_pos = vmath.vector3(0, 0, 0),
-    eye_pos = vmath.vector3(0, 70, 0),
+    eye_pos = vmath.vector3(0, 170, 0),
     yaw = 270,
-    pitch = 0,
+    pitch = 0
 }
 
 return M
