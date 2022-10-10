d32f3629497d969e8b68065fda8f404e6e111278
Artsiom Trubchyk <aglitchman@gmail.com>
2022-10-10 22:04:01 +0300

    Update to use new API of Defold 1.3.7 BETA

--- pointer_lock/pointer.lua
+++ pointer_lock/pointer.lua
@@ -5,9 +5,14 @@ M.DISPLAY_HEIGHT = tonumber(sys.get_config("display.height"))
 
 M.locked = false
 
+--- Virtual Cursor
+-- X/Y
 M.cursor_x = M.DISPLAY_WIDTH / 2
 M.cursor_y = M.DISPLAY_HEIGHT / 2
+-- Visible or not
 M.cursor_visible = true
+-- GUI Render Order. 15 is top.
+M.cursor_render_order = 15
 
 function M.transform_input(action_id, action)
     action.x = M.cursor_x
